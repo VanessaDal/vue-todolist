bd5ceca24de24f1d2dc1397682abbb99e406e02c
Vanessa D'Alessandro <leavesthatfall@gmail.com>
2020-11-18 20:33:56 +0100

    wip

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// Creiamo una mini app di todolist con html+css+js in Vue
+// Features principali:
+// un input permette all’utente di scrivere una “cosa da fare”;
+// un bottone permette di aggiungere quella cosa alla lista di cose da fare in pagina, come promemoria;
+// una volta che l’utente ha ipoteticamente svolto l’attività, grazie ad un bottone (X) può eliminare quella determinata cosa dalla lista.
+
+var app=new Vue({
+    el: '#app',
+    data:{
+        todo:[],
+        text:""
+    },
+    methods:{
+        submitText:function(){
+            this.todo.push(this.text);
+            this.text="";
+            console.log(this.todo)
+        }
+    }
+})
